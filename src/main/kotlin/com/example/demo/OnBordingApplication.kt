428e8e00b06f4b30b6de54600da938eb6747e068
고병욱 <rhquddnr95@naver.com>
2024-11-18 22:12:25 +0900

    fix: main class renaming #2

--- src/main/kotlin/com/example/demo/OnBordingApplication.kt
+++ src/main/kotlin/com/example/demo/OnBordingApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class OnBordingApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+	runApplication<OnBordingApplication>(*args)
 }
