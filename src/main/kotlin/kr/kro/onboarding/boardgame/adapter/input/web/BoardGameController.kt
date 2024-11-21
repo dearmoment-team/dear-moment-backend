6d27bd4e72570009eb3557cef8bd3e0926495f34
SeongminKimKR <gvf156@gmail.com>
2024-11-21 19:11:07 +0900

    feat: 공통 응답 래핑을 위해 ResponseBodyAdvice 구현

---
+++ src/main/kotlin/kr/kro/onboarding/boardgame/adapter/input/web/BoardGameController.kt
@@ -0,0 +1,10 @@
+package kr.kro.onboarding.boardgame.adapter.input.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class BoardGameController {
+    @GetMapping("/health")
+    fun healthCheck() = "OK"
+}
