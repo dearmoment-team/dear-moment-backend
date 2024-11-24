23bc3ae85297e604310cf9691a92d107d3a5d327
SeongminKimKR <gvf156@gmail.com>
2024-11-24 18:43:11 +0900

    feat: ResponseWrapper 커스텀 컨버터 지원하도록 저용

--- src/main/kotlin/kr/kro/onboarding/boardgame/adapter/output/persistence/BoardGameRepository.kt
+++ src/main/kotlin/kr/kro/onboarding/boardgame/adapter/output/persistence/BoardGameRepository.kt
@@ -3,5 +3,4 @@ package kr.kro.onboarding.boardgame.adapter.output.persistence
 import kr.kro.onboarding.boardgame.adapter.output.persistence.entity.BoardGameEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BoardGameRepository : JpaRepository<BoardGameEntity, Long> {
-}
+interface BoardGameRepository : JpaRepository<BoardGameEntity, Long>
