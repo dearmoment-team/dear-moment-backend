7988d31782a6586a11f734f92a3d2bc70f7127f0
Hojun Song <bmp.tom@yahoo.com>
2025-01-10 21:40:50 +0900

    feat : docker file

--- src/main/kotlin/kr/kro/onboarding/common/config/WebConfig.kt
+++ src/main/kotlin/kr/kro/onboarding/common/config/WebConfig.kt
@@ -11,21 +11,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig(
     private val objectMapper: ObjectMapper,
 ) : WebMvcConfigurer {
+    companion object {
+        private const val HIGHEST_CONVERTER_PRIORITY = 0
+    }
+
     override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
-        // 커스텀 컨버터 제거
+        // 커스텀 컨버터 추가
+        println("[DEBUG] WebConfig.extendMessageConverters() 호출됨")
         converters.add(HIGHEST_CONVERTER_PRIORITY, CustomHttpMessageConverter(objectMapper))
     }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        println("[DEBUG] WebConfig.addResourceHandlers() 호출됨")
+
+        // "/static/**" → classpath:/static/
+        println("[DEBUG] '/static/**' → 'classpath:/static/' 매핑 추가")
         registry.addResourceHandler("/static/**")
             .addResourceLocations("classpath:/static/")
-        registry.addResourceHandler("swagger-ui.html")
+
+        // "/swagger-ui.html" → classpath:/static/swagger-ui/
+        println("[DEBUG] 'swagger-ui.html' → 'classpath:/static/swagger-ui/' 매핑 추가")
+        registry.addResourceHandler("/swagger-ui.html")
             .addResourceLocations("classpath:/static/swagger-ui/")
+
+        // "/swagger-ui/**" → classpath:/static/swagger-ui/
+        println("[DEBUG] '/swagger-ui/**' → 'classpath:/static/swagger-ui/' 매핑 추가")
         registry.addResourceHandler("/swagger-ui/**")
             .addResourceLocations("classpath:/static/swagger-ui/")
-    }
-
-    companion object {
-        private const val HIGHEST_CONVERTER_PRIORITY = 0
     }
 }
