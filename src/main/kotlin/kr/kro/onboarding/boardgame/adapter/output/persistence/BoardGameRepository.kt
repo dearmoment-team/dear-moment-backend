9cf1e64b8dbdc5d218211468abce3dbcf2ff9c8f
seongmin <gvf156@gmail.com>
2025-01-18 12:10:34 +0900

    remove: 미사용 예정 파일 정리

--- src/main/kotlin/kr/kro/onboarding/boardgame/adapter/output/persistence/BoardGameRepository.kt
+++
@@ -1,6 +0,0 @@
-package kr.kro.onboarding.boardgame.adapter.output.persistence
-
-import kr.kro.onboarding.boardgame.adapter.output.persistence.entity.BoardGameEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BoardGameRepository : JpaRepository<BoardGameEntity, Long>
