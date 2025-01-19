42ced3dd6185a67982338f42f6cf6c863abcc3cb
seongmin <gvf156@gmail.com>
2025-01-19 15:32:41 +0900

    feat: ResponWraaper 응답 상태 코드 2xx  BaseResponse.success() 리턴하도록 수정

--- src/main/kotlin/kr/kro/onboarding/common/dto/ResponseWrapper.kt
+++ src/main/kotlin/kr/kro/onboarding/common/dto/ResponseWrapper.kt
@@ -33,7 +33,7 @@ class ResponseWrapper : ResponseBodyAdvice<Any> {
         val resolvedStatus = HttpStatus.resolve(status)
 
         return if (resolvedStatus?.is2xxSuccessful == true) {
-            BaseResponse(success = true, code = status, data = body)
+            BaseResponse.success(data = body)
         } else {
             print("Not wrapping response due to non-2xx status\n")
             BaseResponse.error(code = status)
