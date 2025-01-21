7ad18d9fcdf1b55ebcdb08c0597f34b93dbc850f
Hojun Song <bmp.tom@yahoo.com>
2025-01-21 22:25:26 +0900

    feat: 상품 저장소 인터페이스와 관련 클래스의 패키지를 변경합니다. #14

--- src/main/kotlin/kr/kro/dearmoment/product/adapter/out/persistence/ProductRepositoryAdapter.kt
+++ src/main/kotlin/kr/kro/dearmoment/product/adapter/out/persistence/ProductRepositoryAdapter.kt
@@ -1,14 +1,13 @@
-package kr.kro.dearmoment.product.adapter.persistence
+package kr.kro.dearmoment.product.adapter.out.persistence
 
-import kr.kro.dearmoment.product.application.port.ProductRepository
+import kr.kro.dearmoment.product.application.port.out.ProductPersistencePort
 import kr.kro.dearmoment.product.domain.model.Product
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 class ProductRepositoryAdapter(
     private val jpaProductRepository: JpaProductRepository,
-) : ProductRepository {
+) : ProductPersistencePort {
     override fun save(product: Product): Product {
         val entity = ProductEntity.fromDomain(product)
         val saved = jpaProductRepository.save(entity)
@@ -25,5 +24,3 @@ class ProductRepositoryAdapter(
             .map { it.toDomain() }
     }
 }
-
-interface JpaProductRepository : JpaRepository<ProductEntity, Long>
