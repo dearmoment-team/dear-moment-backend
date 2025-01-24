404a37cdee05c5615ae13f87d912c00c684a26f6
Hojun Song <bmp.tom@yahoo.com>
2025-01-24 22:04:10 +0900

    feat: 상품 옵션 저장소의 어댑터 로직에서 유즈케이스를 분리합니다. #14

--- src/test/kotlin/kr/kro/dearmoment/product/application/ProductOptionUseCaseTest.kt
+++ src/test/kotlin/kr/kro/dearmoment/product/application/ProductOptionUseCaseTest.kt
@@ -6,6 +6,8 @@ import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kr.kro.dearmoment.product.adapter.out.persistence.ProductEntity
+import kr.kro.dearmoment.product.application.port.out.ProductEntityRetrievalPort
 import kr.kro.dearmoment.product.domain.model.ProductOption
 import kr.kro.dearmoment.product.application.port.out.ProductOptionPersistencePort
 import kr.kro.dearmoment.product.application.usecase.ProductOptionUseCase
@@ -15,18 +17,21 @@ class ProductOptionUseCaseTest : StringSpec({
 
     // Mock 초기화
     val productOptionPersistencePort: ProductOptionPersistencePort = mockk()
-    val productOptionUseCase = ProductOptionUseCase(productOptionPersistencePort)
+    val productEntityRetrievalPort: ProductEntityRetrievalPort = mockk()
+    val productOptionUseCase = ProductOptionUseCase(productOptionPersistencePort, productEntityRetrievalPort)
 
     "중복된 옵션 이름 저장 시 예외 발생" {
         // given
         val fixedNow = LocalDateTime.of(2025, 1, 1, 12, 0)
+        val productEntity = mockk<ProductEntity>()
         val existingOptions = listOf(
             ProductOption(1L, 1L, "중복 옵션", 5000, "중복 옵션 설명", fixedNow, fixedNow)
         )
         val newOption = ProductOption(2L, 1L, "중복 옵션", 10000, "또 다른 중복 옵션 설명", fixedNow, fixedNow)
 
-        // Mock 설정: findByProductId 호출 시 기존 옵션 반환
-        every { productOptionPersistencePort.findByProductId(1L) } returns existingOptions
+        // Mock 설정: getProductEntityById 호출 및 findByProduct 동작 설정
+        every { productEntityRetrievalPort.getProductEntityById(1L) } returns productEntity
+        every { productOptionPersistencePort.findByProduct(productEntity) } returns existingOptions
 
         // when & then
         val exception = shouldThrow<IllegalArgumentException> {
@@ -37,6 +42,7 @@ class ProductOptionUseCaseTest : StringSpec({
         exception.message shouldBe "Duplicate option name: 중복 옵션"
 
         // 호출 검증
-        verify(exactly = 1) { productOptionPersistencePort.findByProductId(1L) }
+        verify(exactly = 1) { productEntityRetrievalPort.getProductEntityById(1L) }
+        verify(exactly = 1) { productOptionPersistencePort.findByProduct(productEntity) }
     }
 })
