745f7da3bf04b2b777354204a777bc6731242e8c
Hojun Song <bmp.tom@yahoo.com>
2025-01-28 15:28:23 +0900

    fix: 테스트코드를 재작성하여 가독성을 향상시킵니다. #14

--- src/main/kotlin/kr/kro/dearmoment/product/application/port/out/ProductOptionPersistencePort.kt
+++ src/main/kotlin/kr/kro/dearmoment/product/application/port/out/ProductOptionPersistencePort.kt
@@ -4,11 +4,50 @@ import kr.kro.dearmoment.product.domain.model.Product
 import kr.kro.dearmoment.product.domain.model.ProductOption
 
 interface ProductOptionPersistencePort {
+
+    /**
+     * ProductOption을 저장합니다.
+     * @param productOption 저장할 ProductOption 도메인 모델
+     * @return 저장된 ProductOption 도메인 모델
+     */
     fun save(productOption: ProductOption): ProductOption
+
+    /**
+     * ID를 기준으로 ProductOption을 조회합니다.
+     * @param id 조회할 ProductOption의 ID
+     * @return 조회된 ProductOption 도메인 모델
+     */
     fun findById(id: Long): ProductOption
+
+    /**
+     * 모든 ProductOption을 조회합니다.
+     * @return 조회된 ProductOption 도메인 모델 리스트
+     */
     fun findAll(): List<ProductOption>
+
+    /**
+     * ID를 기준으로 ProductOption을 삭제합니다.
+     * @param id 삭제할 ProductOption의 ID
+     */
     fun deleteById(id: Long)
+
+    /**
+     * 특정 Product에 속한 모든 ProductOption을 조회합니다.
+     * @param product 조회할 Product 도메인 모델
+     * @return 해당 Product에 속한 ProductOption 도메인 모델 리스트
+     */
     fun findByProduct(product: Product): List<ProductOption>
+
+    /**
+     * 특정 Product ID에 속한 모든 ProductOption을 삭제합니다.
+     * @param productId 삭제할 Product ID
+     */
     fun deleteAllByProductId(productId: Long)
+
+    /**
+     * 특정 Product ID에 속한 ProductOption의 존재 여부를 확인합니다.
+     * @param productId 확인할 Product ID
+     * @return ProductOption 존재 여부
+     */
     fun existsByProductId(productId: Long): Boolean
 }
