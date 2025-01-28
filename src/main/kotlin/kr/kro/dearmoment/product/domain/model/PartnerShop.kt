ad1ec603d3b084b05b742f0422d94f16e7f6444f
Hojun Song <bmp.tom@yahoo.com>
2025-01-28 18:45:03 +0900

    fix: model부분 테스트 코드 및 실제 코드 작성 #14

--- src/main/kotlin/kr/kro/dearmoment/product/domain/model/PartnerShop.kt
+++ src/main/kotlin/kr/kro/dearmoment/product/domain/model/PartnerShop.kt
@@ -2,7 +2,7 @@ package kr.kro.dearmoment.product.domain.model
 
 data class PartnerShop(
     val name: String,
-    val link: String
+    val link: String,
 ) {
     init {
         require(name.isNotBlank()) { "파트너샵 이름은 비어 있을 수 없습니다." }
