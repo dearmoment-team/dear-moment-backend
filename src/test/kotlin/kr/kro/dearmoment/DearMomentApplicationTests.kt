43f9b13d591831e06ff54a831437efe9430124a1
SeongminKimKR <gvf156@gmail.com>
2025-02-02 18:13:59 +0900

    test: 테스트 중복 설정 리팩토링 #18
    - refactor: 각 레이어별 어노테이션 추출
    - refactor: 컨트롤러 공통 설정 추출

--- src/test/kotlin/kr/kro/dearmoment/DearMomentApplicationTests.kt
+++ src/test/kotlin/kr/kro/dearmoment/DearMomentApplicationTests.kt
@@ -1,11 +1,8 @@
 package kr.kro.dearmoment
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
-@ActiveProfiles("test")
+@IntegrationTest
 class DearMomentApplicationTests {
     @Test
     fun contextLoads() {
