4cead39f60087fdd29955cea2c5ed9abf340d37c
SeongminKimKR <gvf156@gmail.com>
2025-02-08 23:40:28 +0900

    feat: 좋아요 영속성 어댑터 작성 #24

--- src/test/kotlin/kr/kro/dearmoment/like/domain/LikeTest.kt
+++ src/test/kotlin/kr/kro/dearmoment/like/domain/LikeTest.kt
@@ -6,12 +6,6 @@ import io.kotest.core.spec.style.DescribeSpec
 class LikeTest : DescribeSpec({
     describe("좋아요는") {
         val likeType = LikeType.AUTHOR
-        context("생성 시 좋아요 ID가 양수가 아니면") {
-            it("예외를 발생시킨다.") {
-                shouldThrow<IllegalArgumentException> { Like(id = -1, userId = 1, targetId = 1, type = likeType) }
-                shouldThrow<IllegalArgumentException> { Like(id = 0, userId = 1, targetId = 1, type = likeType) }
-            }
-        }
 
         context("생성 시 유저 ID가 양수가 아니면") {
             it("예외를 발생시킨다.") {
