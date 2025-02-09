68592fdff19b1e84935dce2fdcfb430c3e465b28
SeongminKimKR <gvf156@gmail.com>
2025-02-09 15:38:35 +0900

    feat: 객체 스토리지로 부터 특정 개체 public url 생성, 삭제 기능 구현

--- src/main/kotlin/kr/kro/dearmoment/image/adapter/output/objectstorage/OracleObjectStorageAdapter.kt
+++ src/main/kotlin/kr/kro/dearmoment/image/adapter/output/objectstorage/OracleObjectStorageAdapter.kt
@@ -1,21 +1,29 @@
 package kr.kro.dearmoment.image.adapter.output.objectstorage
 
 import com.oracle.bmc.Region
+import com.oracle.bmc.objectstorage.model.CreatePreauthenticatedRequestDetails
+import com.oracle.bmc.objectstorage.model.CreatePreauthenticatedRequestDetails.AccessType
+import com.oracle.bmc.objectstorage.requests.CreatePreauthenticatedRequestRequest
 import com.oracle.bmc.objectstorage.requests.DeleteObjectRequest
+import com.oracle.bmc.objectstorage.requests.DeletePreauthenticatedRequestRequest
 import com.oracle.bmc.objectstorage.requests.PutObjectRequest
 import com.oracle.bmc.objectstorage.transfer.UploadManager.UploadRequest
 import kr.kro.dearmoment.image.application.command.SaveImageCommand
 import kr.kro.dearmoment.image.application.port.output.DeleteImageFromObjectStoragePort
+import kr.kro.dearmoment.image.application.port.output.GetImageFromObjectStoragePort
 import kr.kro.dearmoment.image.application.port.output.UploadImagePort
 import kr.kro.dearmoment.image.domain.Image
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
+import java.time.ZoneId
+import java.util.Date
 import java.util.UUID
 
 @Component
 class OracleObjectStorageAdapter(
     private val objectStorageProperties: OracleObjectStorageProperties,
-) : UploadImagePort, DeleteImageFromObjectStoragePort {
+    private val objectStorageUtil: OracleObjectStorageUtil,
+) : UploadImagePort, DeleteImageFromObjectStoragePort, GetImageFromObjectStoragePort {
     private val baseUrl =
         "https://" + objectStorageProperties.namespaceName +
             ".objectstorage." + Region.AP_CHUNCHEON_1.regionId + ".oci.customer-oci.com"
@@ -44,40 +52,85 @@ class OracleObjectStorageAdapter(
                 .allowOverwrite(true)
                 .build(putRequest)
 
-        OracleObjectStorageUtil().uploadManager.upload(uploadRequest)
+        objectStorageUtil.uploadManager.upload(uploadRequest)
 
-        val url = "$baseUrl/$fileName"
+        val image =
+            Image(
+                userId = userId,
+                fileName = fileName,
+            )
 
-        return toDomain(userId, url, fileName)
+        return getImage(image)
     }
 
     override fun uploadAll(commands: List<SaveImageCommand>): List<Image> {
         return commands.map { upload(it.file, it.userId) }
     }
 
-    override fun delete(fileName: String) {
+    override fun delete(image: Image) {
         val request =
             DeleteObjectRequest.builder()
                 .namespaceName(objectStorageProperties.namespaceName)
                 .bucketName(objectStorageProperties.bucketName)
-                .objectName(fileName)
+                .objectName(image.fileName)
                 .build()
+
+        deletePreAuth(image.parId)
 
         val client = OracleObjectStorageUtil().client
         client.deleteObject(request)
     }
 
+    override fun getImage(image: Image): Image {
+        deletePreAuth(image.parId)
+
+        val expireTime = Date(System.currentTimeMillis() + TWO_HOURS_FOR_SECONDS)
+
+        val details =
+            CreatePreauthenticatedRequestDetails.builder()
+                .accessType(AccessType.ObjectReadWrite)
+                .objectName(image.fileName)
+                .timeExpires(expireTime)
+                .name(image.fileName)
+                .build()
+
+        val request =
+            CreatePreauthenticatedRequestRequest.builder()
+                .namespaceName(objectStorageProperties.namespaceName)
+                .bucketName(objectStorageProperties.bucketName)
+                .createPreauthenticatedRequestDetails(details)
+                .build()
+
+        val response = objectStorageUtil.client.createPreauthenticatedRequest(request)
+        val url = "$baseUrl/${response.preauthenticatedRequest.accessUri}"
+
+        return Image(
+            imageId = image.imageId,
+            userId = image.userId,
+            parId = response.preauthenticatedRequest.id,
+            fileName = image.fileName,
+            url = url,
+            urlExpireTime =
+                expireTime.toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime(),
+        )
+    }
+
+    private fun deletePreAuth(parId: String) {
+        if (parId.isEmpty()) return
+
+        val request =
+            DeletePreauthenticatedRequestRequest.builder()
+                .namespaceName(objectStorageProperties.namespaceName)
+                .bucketName(objectStorageProperties.bucketName)
+                .parId(parId)
+                .build()
+
+        objectStorageUtil.client.deletePreauthenticatedRequest(request)
+    }
+
     companion object {
-        fun toDomain(
-            userId: Long,
-            url: String,
-            fileName: String,
-        ): Image {
-            return Image(
-                userId = userId,
-                url = url,
-                fileName = fileName,
-            )
-        }
+        private const val TWO_HOURS_FOR_SECONDS = 2 * 60 * 60 * 1000
     }
 }
--- src/main/kotlin/kr/kro/dearmoment/image/application/port/output/GetImageFromObjectStoragePort.kt
+++ src/main/kotlin/kr/kro/dearmoment/image/application/port/output/GetImageFromObjectStoragePort.kt
@@ -2,6 +2,6 @@ package kr.kro.dearmoment.image.application.port.output
 
 import kr.kro.dearmoment.image.domain.Image
 
-interface DeleteImageFromObjectStoragePort {
-    fun delete(image: Image)
+interface GetImageFromObjectStoragePort {
+    fun getImage(image: Image): Image
 }
