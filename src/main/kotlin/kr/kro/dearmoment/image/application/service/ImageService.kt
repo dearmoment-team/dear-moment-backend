05f0e4c03cfc54ff532e41fcf5ce3e1a3a0527cb
SeongminKimKR <gvf156@gmail.com>
2025-02-09 15:38:35 +0900

    feat: 객체 스토리지로 부터 특정 개체 public url 생성, 삭제 기능 구현

--- src/main/kotlin/kr/kro/dearmoment/image/application/service/ImageService.kt
+++ src/main/kotlin/kr/kro/dearmoment/image/application/service/ImageService.kt
@@ -8,6 +8,7 @@ import kr.kro.dearmoment.image.application.port.input.GetImageUseCase
 import kr.kro.dearmoment.image.application.port.input.SaveImageUseCase
 import kr.kro.dearmoment.image.application.port.output.DeleteImageFromDBPort
 import kr.kro.dearmoment.image.application.port.output.DeleteImageFromObjectStoragePort
+import kr.kro.dearmoment.image.application.port.output.GetImageFromObjectStoragePort
 import kr.kro.dearmoment.image.application.port.output.GetImagePort
 import kr.kro.dearmoment.image.application.port.output.SaveImagePort
 import kr.kro.dearmoment.image.application.port.output.UploadImagePort
@@ -20,6 +21,7 @@ class ImageService(
     private val saveImagePort: SaveImagePort,
     private val getImagePort: GetImagePort,
     private val deleteImageFromDBPort: DeleteImageFromDBPort,
+    private val getImageFromObjectStorage: GetImageFromObjectStoragePort,
     private val deleteImageFromObjectStorage: DeleteImageFromObjectStoragePort,
 ) : SaveImageUseCase, DeleteImageUseCase, GetImageUseCase {
     @Transactional
@@ -37,20 +39,35 @@ class ImageService(
     @Transactional(readOnly = true)
     override fun getOne(imageId: Long): GetImageResponse {
         val image = getImagePort.findOne(imageId)
+
+        if (image.isUrlExpired()) {
+            return GetImageResponse.from(getImageFromObjectStorage.getImage(image))
+        }
+
         return GetImageResponse.from(image)
     }
 
     @Transactional(readOnly = true)
     override fun getAll(userId: Long): GetImagesResponse {
         val images = getImagePort.findAll(userId)
-        return GetImagesResponse.from(images)
+
+        val finalResult =
+            images.map { image ->
+                if (image.isUrlExpired()) {
+                    getImageFromObjectStorage.getImage(image)
+                } else {
+                    image
+                }
+            }
+
+        return GetImagesResponse.from(finalResult)
     }
 
     @Transactional
     override fun delete(imageId: Long) {
         val image = getImagePort.findOne(imageId)
 
-        deleteImageFromObjectStorage.delete(image.fileName)
+        deleteImageFromObjectStorage.delete(image)
         deleteImageFromDBPort.delete(imageId)
     }
 }
