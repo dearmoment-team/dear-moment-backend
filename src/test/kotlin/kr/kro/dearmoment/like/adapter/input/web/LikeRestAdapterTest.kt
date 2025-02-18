b1cc56059f8b23183ba7295f522965d4ae564c1b
SeongminKimKR <67994481+SeongminKimKR@users.noreply.github.com>
2025-02-19 01:04:53 +0900

    fix: conflict 수정

--- src/test/kotlin/kr/kro/dearmoment/like/adapter/input/web/LikeRestAdapterTest.kt
+++ src/test/kotlin/kr/kro/dearmoment/like/adapter/input/web/LikeRestAdapterTest.kt
@@ -17,7 +17,7 @@ import kr.kro.dearmoment.common.restdocs.toJsonString
 import kr.kro.dearmoment.common.restdocs.type
 import kr.kro.dearmoment.like.adapter.input.web.dto.LikeRequest
 import kr.kro.dearmoment.like.adapter.input.web.dto.LikeResponse
-import kr.kro.dearmoment.like.application.command.LikeCommand
+import kr.kro.dearmoment.like.application.command.SaveLikeCommand
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
