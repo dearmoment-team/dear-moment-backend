8e5faa4c1db95db6dec332017444408dee135b5f
SeongminKimKR <gvf156@gmail.com>
2025-03-01 03:20:45 +0900

    test:테스트 주석

--- src/test/kotlin/kr/kro/dearmoment/image/adapter/output/objectstorage/OracleObjectStorageAdapterTest.kt
+++ src/test/kotlin/kr/kro/dearmoment/image/adapter/output/objectstorage/OracleObjectStorageAdapterTest.kt
@@ -1,23 +1,17 @@
 import io.kotest.core.spec.style.FunSpec
-import kr.kro.dearmoment.DearMomentApplication
-import kr.kro.dearmoment.image.adapter.output.objectstorage.OracleObjectStorageAdapter
-import kr.kro.dearmoment.image.adapter.output.objectstorage.OracleObjectStorageProperties
-import kr.kro.dearmoment.image.adapter.output.objectstorage.OracleObjectStorageUtil
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mock.web.MockMultipartFile
-import org.springframework.test.context.ActiveProfiles
 import java.io.File
 import java.nio.file.Files
 
-@SpringBootTest(classes = [DearMomentApplication::class])
-@ActiveProfiles("test")
+// @SpringBootTest(classes = [DearMomentApplication::class])
+// @ActiveProfiles("test")
 class OracleObjectStorageAdapterTest(
-    private val objectStorageProperties: OracleObjectStorageProperties,
+//    private val objectStorageProperties: OracleObjectStorageProperties,
 ) : FunSpec({
         test("upload, delete 테스트") {
-            val objectStorageUtil = OracleObjectStorageUtil(objectStorageProperties)
-            val adapter = OracleObjectStorageAdapter(objectStorageProperties, objectStorageUtil)
-            // Given
+//            val objectStorageUtil = OracleObjectStorageUtil(objectStorageProperties)
+//            val adapter = OracleObjectStorageAdapter(objectStorageProperties, objectStorageUtil)
+//            // Given
             val imageFile = File("src/test/resources/test-image.jpg") // 테스트용 이미지 파일
             val multipartFile =
                 MockMultipartFile(
