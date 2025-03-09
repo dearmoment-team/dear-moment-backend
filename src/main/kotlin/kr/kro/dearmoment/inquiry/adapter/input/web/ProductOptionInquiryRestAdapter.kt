01ae7d82ee2329df3a3f311233e21d2ecf7de499
SeongminKimKR <gvf156@gmail.com>
2025-03-09 20:22:46 +0900

    docs: 문의 스웨거 문서화

--- src/main/kotlin/kr/kro/dearmoment/inquiry/adapter/input/web/ProductOptionInquiryRestAdapter.kt
+++ src/main/kotlin/kr/kro/dearmoment/inquiry/adapter/input/web/ProductOptionInquiryRestAdapter.kt
@@ -1,5 +1,12 @@
 package kr.kro.dearmoment.inquiry.adapter.input.web
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import kr.kro.dearmoment.common.dto.PagedResponse
 import kr.kro.dearmoment.inquiry.application.dto.CreateInquiryResponse
 import kr.kro.dearmoment.inquiry.application.dto.CreateProductOptionInquiryRequest
@@ -21,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "ProductOptionInquiry API", description = "상품 옵션 문의 관련 API")
 @RestController
 @RequestMapping("/api/inquiries/product-options")
 class ProductOptionInquiryRestAdapter(
@@ -28,13 +36,35 @@ class ProductOptionInquiryRestAdapter(
     private val getInquiryUseCase: GetInquiryUseCase,
     private val removeInquiryUseCase: RemoveInquiryUseCase,
 ) {
+    @Operation(summary = "상품 옵션 문의 생성", description = "새로운 상품 옵션 문의를 생성합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "상품 옵션 문의 생성 성공",
+                content = [Content(schema = Schema(implementation = CreateInquiryResponse::class))],
+            ),
+        ],
+    )
     @PostMapping
     fun writeProductInquiry(
+        @Parameter(description = "생성할 상품 옵션 문의 정보", required = true)
         @RequestBody request: CreateProductOptionInquiryRequest,
     ): CreateInquiryResponse = createInquiryUseCase.createProductInquiry(request.toCommand())
 
+    @Operation(summary = "유저 상품 옵션 문의 조히", description = "유저가 등록한 상품 옵션 문의를 모두 조회합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "상품 옵션 문의 조회 성공",
+                content = [Content(schema = Schema(implementation = PagedResponse::class))],
+            ),
+        ],
+    )
     @GetMapping("/{userId}")
     fun getProductInquiries(
+        @Parameter(description = "조회할 유저의 식별자", required = true)
         @PathVariable userId: Long,
         @PageableDefault(size = 10, sort = ["createdDate"], direction = Sort.Direction.DESC) pageable: Pageable,
     ): PagedResponse<GetProductOptionInquiryResponse> {
@@ -42,6 +72,15 @@ class ProductOptionInquiryRestAdapter(
         return getInquiryUseCase.getProductOptionInquiries(query)
     }
 
+    @Operation(summary = "유저 상품 옵션 문의 삭제", description = "유저가 등록한 상품 옵션 문의를 삭제합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "삭제 성공",
+            ),
+        ],
+    )
     @DeleteMapping("/{inquiryId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun removeProductInquiry(
