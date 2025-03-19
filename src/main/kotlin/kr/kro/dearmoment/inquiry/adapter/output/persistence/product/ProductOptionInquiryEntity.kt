7f3463eef4f50daaabd5cb2a94f43aa9d984a73e
SeongminKimKR <gvf156@gmail.com>
2025-03-20 00:47:40 +0900

    remove: 미사용 메서드 제거

--- src/main/kotlin/kr/kro/dearmoment/inquiry/adapter/output/persistence/product/ProductOptionInquiryEntity.kt
+++ src/main/kotlin/kr/kro/dearmoment/inquiry/adapter/output/persistence/product/ProductOptionInquiryEntity.kt
@@ -29,13 +29,6 @@ class ProductOptionInquiryEntity(
     @JoinColumn(name = "option_id")
     val option: ProductOptionEntity,
 ) : Auditable() {
-    fun toCreateDomain(): CreateProductOptionInquiry =
-        CreateProductOptionInquiry(
-            id = id,
-            userId = userId,
-            productOptionId = option.optionId,
-        )
-
     fun toDomain(): ProductOptionInquiry {
         val product = option.product
         val studio = product.studio ?: throw CustomException(ErrorCode.STUDIO_NOT_FOUND)
