dff853c29d431292768850867b8c0ba9f3bb0d09
고병욱 <rhquddnr95@naver.com>
2025-04-04 11:49:26 +0900

    fix: 유저 엔티티 테스트 wanning 제거

--- src/test/kotlin/kr/kro/dearmoment/user/domain/UserTest.kt
+++ src/test/kotlin/kr/kro/dearmoment/user/domain/UserTest.kt
@@ -11,7 +11,6 @@ class UserTest : DescribeSpec({
         context("모든 필드가 정상적이면") {
             it("정상적으로 객체가 생성된다.") {
                 val now = LocalDateTime.now()
-                val user =
                     User(
                         id = null,
                         loginId = "testUser",
@@ -27,8 +26,6 @@ class UserTest : DescribeSpec({
         context("모든 필드가 일부만 있더라도") {
             it("null 허용 필드는 없어도 정상 생성된다.") {
                 val now = LocalDateTime.now()
-                // isStudio, updatedAt, updatedUser를 null로
-                val user =
                     User(
                         id = null,
                         loginId = "anotherUser",
@@ -40,8 +37,6 @@ class UserTest : DescribeSpec({
                     )
             }
         }
-
-        // ---- 아래는 필수 필드 검증 테스트 ----
 
         context("loginId가 비어있으면") {
             it("예외를 발생시킨다.") {
@@ -58,8 +53,6 @@ class UserTest : DescribeSpec({
                 }
             }
         }
-
-        // ... 비밀번호, name, createdUser 등 필수 값 테스트 동일
 
         context("updatedAt이 존재하지만 createdAt 이후이면") {
             it("예외를 발생시킨다.") {
