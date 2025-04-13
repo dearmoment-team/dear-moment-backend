f02df642850191227ac634031b1ea84f9932be46
SeongminKimKR <gvf156@gmail.com>
2025-04-13 21:36:02 +0900

    fix: SecurityConfig에  CORS 설정 변경 [PATCH 추가]

--- src/main/kotlin/kr/kro/dearmoment/security/SecurityConfig.kt
+++ src/main/kotlin/kr/kro/dearmoment/security/SecurityConfig.kt
@@ -94,7 +94,7 @@ class SecurityConfig(
 
     companion object {
         val ALLOWED_ORIGINS = arrayOf("https://*.kro.kr", "https://*.o-r.kr", "http://localhost:*")
-        val ALLOWED_METHODS = arrayOf("OPTIONS", "GET", "POST", "PUT", "DELETE")
+        val ALLOWED_METHODS = arrayOf("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
         const val AUTHORIZATION_HEADER = "Authorization"
         const val MAX_AGE = 3600L
     }
