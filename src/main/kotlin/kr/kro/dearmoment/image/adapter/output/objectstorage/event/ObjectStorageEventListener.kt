03414932f95aacc7630852df151df516ce655cf7
SeongminKimKR <gvf156@gmail.com>
2025-06-06 14:09:37 +0900

    feat: 문의, 메일 관심사 분리

--- src/main/kotlin/kr/kro/dearmoment/image/adapter/output/objectstorage/event/ObjectStorageEventListener.kt
+++ src/main/kotlin/kr/kro/dearmoment/image/adapter/output/objectstorage/event/ObjectStorageEventListener.kt
@@ -1,6 +1,8 @@
 package kr.kro.dearmoment.image.adapter.output.objectstorage.event
 
 import kr.kro.dearmoment.image.adapter.output.objectstorage.OracleObjectStorageAdapter
+import kr.kro.dearmoment.inquiry.adapter.output.mail.event.InquiryEventListener
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
@@ -10,12 +12,15 @@ import org.springframework.transaction.event.TransactionalEventListener
 class ObjectStorageEventListener(
     private val objectStorageAdapter: OracleObjectStorageAdapter,
 ) {
+    private val logger = LoggerFactory.getLogger(InquiryEventListener::class.java)
+
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun processImageDelete(event: ImageDeleteEvent) {
         try {
             objectStorageAdapter.delete(event.parId, event.fileName)
-        } catch (_: Exception) {
+        } catch (e: Exception) {
+            logger.error("[ObjectStorageEventListener.processImageDelete] fail", e)
         }
     }
 }
